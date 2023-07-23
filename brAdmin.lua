1ced1363e55d7b0dfa9760fb2d0b3fc4670b957d
Fabio Kaufmann <baselzockt@gmail.com>
2023-07-23 14:01:42 +0200

    added launcher

---
+++ brAdmin.lua
@@ -0,0 +1,9 @@
+local args = { ... }
+
+if args[1] == "start" then
+    if args [2] == "master" then
+        shell.run("master/master", args[3])
+    elseif args[1] == "slave" then
+        shell.run("slave/slave", args[3])
+    end
+end

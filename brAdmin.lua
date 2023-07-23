22f9aa00e0db44832b923dfea9ac0d48d1a28053
Fabio Kaufmann <baselzockt@gmail.com>
2023-07-23 14:29:28 +0200

    fixed elif statement

--- brAdmin.lua
+++ brAdmin.lua
@@ -3,7 +3,7 @@ local args = { ... }
 if args[1] == "start" then
     if args [2] == "master" then
         shell.run("master/master", args[3])
-    elseif args[1] == "slave" then
+    elseif args[2] == "slave" then
         shell.run("slave/slave", args[3])
     end
 end
